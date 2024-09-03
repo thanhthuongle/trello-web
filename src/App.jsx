68cc13a480b3d500ea9e8211172d6fe449fbb28e
thanhthuongle <thuong.lt215485@sis.hust.edu.vn>
2024-09-03 17:49:02 +0700

    Config MUI Theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
@@ -25,6 +26,9 @@ function App() {
       <HomeIcon color="action" />
       <HomeIcon color="disabled" />
       <HomeIcon sx={{ color: pink[500] }} />
+
+      <br />
+      <Typography variant='body2' color='text.secondary'>Test custom theme</Typography>
     </>
   )
 }
