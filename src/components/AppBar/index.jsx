aaa17ef41b294de3170b26eaf4e123f70a577c34
thanhthuongle <thuong.lt215485@sis.hust.edu.vn>
2024-09-07 17:42:44 +0700

    App Bar Component Part 2

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -28,8 +28,8 @@ const AppBar = () => {
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }} />
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-          <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color: 'primary.main' }}/>
-          <Typography variant='span' sx={{ fontSize: '1.1rem', fontWeight: 'bold', color: 'primary.main' }} > Trello </Typography>
+          <SvgIcon component={TrelloIcon} fontSize='small' inheritViewBox sx={{ color: 'primary.main' }}/>
+          <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }} > Trello </Typography>
         </Box>
 
         <Workspaces />
@@ -44,11 +44,11 @@ const AppBar = () => {
         <ModeSelect />
         <Tooltip title="Notification" >
           <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }}>
-            <NotificationsNoneIcon />
+            <NotificationsNoneIcon sx={{ color: 'primary.main' }} />
           </Badge>
         </Tooltip>
         <Tooltip title="Help" >
-          <HelpOutlineIcon sx={{ cursor: 'pointer' }} />
+          <HelpOutlineIcon sx={{ cursor: 'pointer', color: 'primary.main' }} />
         </Tooltip>
         <Profile />
       </Box>
