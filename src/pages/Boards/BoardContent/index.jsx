a85c418fb4718e3dda197016642a720e95b6653a
thanhthuongle <thuong.lt215485@sis.hust.edu.vn>
2024-09-07 14:55:18 +0700

    App Bar Component Part 1

--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -2,7 +2,7 @@ import Box from '@mui/material/Box'
 
 const BoardContent = () => {
   return (
-    <Box sx={{
+    <Box px={2} sx={{
       backgroundColor: 'primary.main',
       width: '100%',
       height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
