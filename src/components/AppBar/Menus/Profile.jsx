de242526b03c631fddcd73edb045664c1f93320d
thanhthuongle <thuong.lt215485@sis.hust.edu.vn>
2024-09-17 22:33:44 +0700

    Board Bar

--- src/components/AppBar/Menus/Profile.jsx
+++ src/components/AppBar/Menus/Profile.jsx
@@ -34,7 +34,7 @@ function Profile() {
           aria-expanded={open ? 'true' : undefined}
         >
           <Avatar
-            sx={{ width: 32, height: 32 }}
+            sx={{ width: 34, height: 34 }}
             alt='Sugiahoabinh'
             src='/src/assets/avatar.jpg'
           />
--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,15 +1,118 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLE = {
+  backgroundColor: 'white',
+  color: 'primary.main',
+  border: 'none',
+  // paddingX: '5px',
+  borderRadius: '5px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    backgroundColor: 'primary.50'
+  }
+}
 
 const BoardBar = () => {
   return (
     <Box px={2} sx={{
-      backgroundColor: 'primary.dark',
+      // backgroundColor: 'primary.dark',
       width: '100%',
       height: (theme) => theme.trello.boardBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5'
     }}>
-      Board Bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={MENU_STYLE}
+          icon={<DashboardIcon />}
+          label="TrungQuanDev MERN STACK board"
+          clickable
+        />
+
+        <Chip
+          sx={MENU_STYLE}
+          icon={<VpnLockIcon />}
+          label="Public/private Worksapce"
+          clickable
+        />
+
+        <Chip
+          sx={MENU_STYLE}
+          icon={<AddToDriveIcon />}
+          label="Add To Google Drive"
+          clickable
+        />
+
+        <Chip
+          sx={MENU_STYLE}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+
+        <Chip
+          sx={MENU_STYLE}
+          icon={<FilterListIcon />}
+          label="Filtes"
+          clickable
+        />
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon />}> Invite </Button>
+        <AvatarGroup
+          max={7}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: '34px',
+              height: '34px',
+              fontSize: '16px'
+            }
+          }}
+        >
+          <Tooltip title='Kirito'>
+            <Avatar alt="Kirito" src="https://i.pinimg.com/originals/d5/99/50/d5995054eadb079af0e1b495c5029b2c.jpg" />
+          </Tooltip>
+          <Tooltip title='Alice'>
+            <Avatar alt="Alice" src="https://i.pinimg.com/originals/4f/eb/67/4feb67fec07558c85a2491ce7ae83685.jpg" />
+          </Tooltip>
+          <Tooltip title='Asuna'>
+            <Avatar alt="Asuna" src="https://i.pinimg.com/originals/17/aa/fb/17aafb3574aea652e5a06a05d0114e31.jpg" />
+          </Tooltip>
+          <Tooltip title='Sinon'>
+            <Avatar alt="Sinon" src="https://i.pinimg.com/originals/92/41/95/924195f5d4c7b19013655ce8d9486383.gif" />
+          </Tooltip>
+          <Tooltip title='Eugeo'>
+            <Avatar alt="Eugeo" src="https://i.pinimg.com/originals/bb/fb/b3/bbfbb372d5a75c9843bd525a402b45f2.jpg" />
+          </Tooltip>
+          <Tooltip title='Yui'>
+            <Avatar alt="Yui" src="https://i.pinimg.com/originals/e5/8d/04/e58d04ef8008cf28e7df29217b57cecf.jpg" />
+          </Tooltip>
+          <Tooltip title='Nobuyuki Sugō'>
+            <Avatar alt="Nobuyuki Sugō" src="https://i.pinimg.com/originals/ef/14/28/ef1428458dfdbfe06fb960c5a46600a0.jpg" />
+          </Tooltip>
+          <Tooltip title='Klein'>
+            <Avatar alt="Klein" src="https://i.pinimg.com/originals/3f/ba/f3/3fbaf38d89296b5c9d6da34f89a549df.jpg" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
