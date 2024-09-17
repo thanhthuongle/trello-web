5744e1a3f83c905225ba5f22d7751c265f31286c
thanhthuongle <thuong.lt215485@sis.hust.edu.vn>
2024-09-17 21:13:33 +0700

    App Bar Component Part 3

--- src/theme.js
+++ src/theme.js
@@ -22,6 +22,23 @@ const theme = extendTheme({
     }
   },
   components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            background: '#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            background: '#00b894'
+          }
+        }
+      }
+    },
     MuiButton: {
       styleOverrides: {
         root: {
