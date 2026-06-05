8bdbde8cb1396e939b0d556313d5c6e7f4007769
Lê Thanh Thương <92387369+thanhthuongle@users.noreply.github.com>
2026-06-05 11:49:20 +0700

    Update author name and registration prompt in LoginForm

--- src/pages/Auth/LoginForm.jsx
+++ src/pages/Auth/LoginForm.jsx
@@ -65,7 +65,7 @@ function LoginForm() {
             <Avatar sx={{ bgcolor: 'primary.main' }}><TrelloIcon /></Avatar>
           </Box>
           <Box sx={{ marginTop: '1em', display: 'flex', justifyContent: 'center', color: theme => theme.palette.grey[500] }}>
-            Author: Sugiahoabinh
+            Author: Le Thanh Thuong
           </Box>
           <Box sx={{ marginTop: '1em', display: 'flex', justifyContent: 'center', flexDirection: 'column', padding: '0 1em' }}>
             {verifiedEmail &&
@@ -135,7 +135,7 @@ function LoginForm() {
             </Button>
           </CardActions>
           <Box sx={{ padding: '0 1em 1em 1em', textAlign: 'center' }}>
-            <Typography>New to Trello MERN Stack Advanced?</Typography>
+            <Typography>Don't have an account?</Typography>
             <Link to="/register" style={{ textDecoration: 'none' }}>
               <Typography sx={{ color: 'primary.main', '&:hover': { color: '#ffbb39' } }}>Create account!</Typography>
             </Link>
